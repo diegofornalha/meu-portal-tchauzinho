53a57faad3d9567062300cd8162e9c70f280e497
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 23:21:18 +0000

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/fcaPobgIvvojSO8k-L-5htzwJLw8Uxuy",
+      url: "https://eth-rinkeby.alchemyapi.io/v2/rzm9Th3XsMB4NpQgIrd4-szgJp7HSHx9",
       accounts: ["8e87f7e6c6717a22064bb73acb6651e68b946c24ebda2e5f8352e35148305249"],
     },
   },
