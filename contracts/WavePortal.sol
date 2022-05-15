ebc0f6777c554b69cf2d8fa249312a12e8143715
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 20:38:49 -0300

    atualizar

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,36 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Ueba, eu sou um contrato e eu sou inteligente");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // Endereço do usuário que deu tchauzinho
+        string message; // Mensagem que o usuário envio
+        uint256 timestamp; // Data/hora de quando o usuário tchauzinhou.
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    constructor() {
+        console.log("EU SOU UM CONTRATO INTELIGENTE. POG.");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s deu tchauzinho!", msg.sender);
+        console.log("%s tchauzinhou com a mensagem %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("Temos um total de %d tchauzinhos!", totalWaves);
+
+        console.log("Temos %d tchauzinhos no total!", totalWaves);
         return totalWaves;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "YOUR_ALCHEMY_API_URL",
-      accounts: ["YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY"],
+      url: "https://eth-rinkeby.alchemyapi.io/v2/rzm9Th3XsMB4NpQgIrd4-szgJp7HSHx9",
+      accounts: ["8e87f7e6c6717a22064bb73acb6651e68b946c24ebda2e5f8352e35148305249"],
     },
   },
 };
