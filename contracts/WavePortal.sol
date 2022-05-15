dea9dbcfdeea2c6548433649577f61dba0255270
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 20:42:38 -0300

    contrato atualizado

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,38 +5,76 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    uint256 totalWaves;
+  
+  struct WaveData {
+    address waver;
+    uint256 timestamp;
+    string message;
+  }
 
-    event NewWave(address indexed from, uint256 timestamp, string message);
+  mapping (address => uint256[]) waverToWaves;
+  WaveData[] waves;
+  uint256 totalWaves;
+  // Utilizaremos isso abaixo para gerar um número randômico
+  uint256 private seed;
 
-    struct Wave {
-        address waver; // Endereço do usuário que deu tchauzinho
-        string message; // Mensagem que o usuário envio
-        uint256 timestamp; // Data/hora de quando o usuário tchauzinhou.
+  event NewWave(address indexed from, uint256 timestamp, string message);
+  
+  constructor() payable {
+    console.log("Contrato publicado!");
+    
+    // Define a semente inicial
+    seed = (block.timestamp + block.difficulty) % 100;
+  }
+
+  function wave(string memory _message) public {
+    
+    if (waverToWaves[msg.sender].length > 0) {
+      uint256 lastWaveIndex = waverToWaves[msg.sender][waverToWaves[msg.sender].length - 1];
+      /*
+      * Precisamos garantir que o valor corrente de timestamp é ao menos 30 segundos maior que o último timestamp armazenado
+      */
+      require(
+        waves[lastWaveIndex].timestamp + 30 seconds < block.timestamp,
+        "Espere 30s para enviar outra transacao"
+      );
     }
+    // waves.length: o índice do próximo salve
+    waverToWaves[msg.sender].push(waves.length);
+    
+    console.log("%s mandou um salve com a mensagem: %s", msg.sender, _message);
+    totalWaves += 1;
+    
+    waves.push(WaveData(msg.sender, block.timestamp, _message));
 
-    Wave[] waves;
+    console.log("%s ja mandou %d salve(s) no total!", msg.sender, waverToWaves[msg.sender].length);
+    
+    emit NewWave(msg.sender, block.timestamp, _message);
 
-    constructor() {
-        console.log("EU SOU UM CONTRATO INTELIGENTE. POG.");
+    // Gera uma nova semente para o próximo que mandar um tchauzinho
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("# randomico gerado: %d", seed);
+
+    // Dá 30%  de chance do usuário ganhar o prêmio.
+    if (seed <= 30) {
+      console.log("%s ganhou!", msg.sender);
+
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+        prizeAmount <= address(this).balance,
+        "Tentando sacar mais dinheiro que o contrato possui."
+      );
+      (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+      require(success, "Falhou em sacar dinheiro do contrato.");
     }
+  }
 
-    function wave(string memory _message) public {
-        totalWaves += 1;
-        console.log("%s tchauzinhou com a mensagem %s", msg.sender, _message);
+  function getAllWaves() public view returns (WaveData[] memory) {
+    return waves;
+  }
 
-        waves.push(Wave(msg.sender, _message, block.timestamp));
-
-        emit NewWave(msg.sender, block.timestamp, _message);
-    }
-
-    function getAllWaves() public view returns (Wave[] memory) {
-        return waves;
-    }
-
-    function getTotalWaves() public view returns (uint256) {
-
-        console.log("Temos %d tchauzinhos no total!", totalWaves);
-        return totalWaves;
-    }
+  function getTotalWaves() public view returns (uint256) {
+    console.log("Temos um total de %d salves!", totalWaves);
+    return totalWaves;
+  }
 }
