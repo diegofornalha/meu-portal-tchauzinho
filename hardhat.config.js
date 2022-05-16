52246869c39596e2b4c35c3022650eca679c63c3
Diego Fornalha <diegofornalha@gmail.com>
2022-05-16 02:12:11 -0300

    criando contrato na goerli e interagindo com ela

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,9 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/_bEj1iL2DfptrO0R2jsDrHAhZPKRP5eU",
-      accounts: ["8e87f7e6c6717a22064bb73acb6651e68b946c24ebda2e5f8352e35148305249"],
+    goerli: {
+      url: "https://eth-goerli.alchemyapi.io/v2/_bEj1iL2DfptrO0R2jsDrHAhZPKRP5eU",
+      accounts: ["ead8546c078edc2522d3130f231e74bf521e29cc2e76d005ce82f93c798873ef"],
     },
   },
 };
